e67378b50e8ae9e09ab2539b7cf1080c34047cfc
Ranset Fleites <ransetfleites0@gmail.com>
2025-11-20 22:16:46 -0500

    Check if wkhtmltopdf is installed

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import jinja2
 import pdfkit
+import shutil
+import os
+
+def comprueba_wkhtmltopdf():
+    # Comprobar si 'wkhtmltopdf' está disponible
+    wkhtml_path = shutil.which('wkhtmltopdf')
+    # Rutas comunes en Windows si no está en PATH
+    if not wkhtml_path:
+        posibles = [
+            r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe',
+            r'C:\Program Files (x86)\wkhtmltopdf\bin\wkhtmltopdf.exe',
+        ]
+        for p in posibles:
+            if os.path.isfile(p):
+                wkhtml_path = p
+                break
+
+    if not wkhtml_path:
+        raise RuntimeError(
+            "wkhtmltopdf no está instalado o no se encuentra en PATH. "
+            "Instale wkhtmltopdf y asegúrese de que 'wkhtmltopdf' esté accesible."
+        )
+    return wkhtml_path
 
 def crea_pdf(info, rutacss=''):
+    
+
     ruta_template = './template1'
     nombre_template = 'index.html'
 
@@ -15,11 +40,16 @@ def crea_pdf(info, rutacss=''):
                 'margin-right': '0.75in',
                 'margin-bottom': '0.75in',
                 'margin-left': '0.75in',
+                'enable-local-file-access': None,
                 # 'footer-center': '[page] of [topage]',
                 # 'footer-line': '',
     }
 
-    config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
+    
+    wkhtml_path = comprueba_wkhtmltopdf()
+
+    config = pdfkit.configuration(wkhtmltopdf=wkhtml_path)
+
     ruta_salida = 'cotizacion.pdf'
 
     pdfkit.from_string(html, ruta_salida, options=opciones, configuration=config)
